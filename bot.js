dc123edf75ac7d3b424d35d5db2ede4af21aac65
AhmadDev2 <53179246+AhmadDev2@users.noreply.github.com>
2019-07-22 15:19:28 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("562584649163997190")
+setInterval(function() {
+channel.send(`hello hello hello i needhelp helllooooo في حدا هون بدي مساعدة ضروري يا عيال وينكم ._.`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
